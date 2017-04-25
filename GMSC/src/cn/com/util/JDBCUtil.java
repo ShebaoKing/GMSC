1c89454da9b23594395a079381ae8acc731bd960
ShebaoKing <582066102@qq.com>
2017-04-25 23:27:37 +0800

    开发完成数据库的链接、增删改查、释放资源

---
+++ GMSC/src/cn/com/util/JDBCUtil.java
@@ -0,0 +1,115 @@
+package cn.com.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+
+
+public class JDBCUtil {
+	
+	private String url="jdbc:mysql://localhost:3306/guimei";
+	
+	private String user="root";
+	
+	private String pwd="redhat";
+	
+	private Connection con=null;
+	
+	private PreparedStatement ps=null;
+	
+	private ResultSet rs=null;
+	//加载驱动
+	static{
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	//获取链接
+	public void getCon(){
+		try {
+			con=DriverManager.getConnection(url, user, pwd);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	//增删改
+	public int update(String sql,Object[] obj){
+		getCon();
+		int n=0;
+		try {
+			ps=con.prepareStatement(sql);
+			for (int i = 0; i < obj.length; i++) {
+				ps.setObject(i+1, obj[i]);
+			}
+			n=ps.executeUpdate();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}finally{
+			//close();
+		}
+		return n;
+	}
+	
+	//查
+	public ResultSet select(String sql,Object[] obj){
+		getCon();		
+		try {
+			ps=con.prepareStatement(sql);
+			for (int i = 0; i < obj.length; i++) {
+				ps.setObject(i+1, obj[i]);
+			}
+			rs=ps.executeQuery();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return rs;
+	}
+	//获取列数
+	public int columnCount(){
+		int colum=0;
+		try {
+			rs.getMetaData().getColumnCount();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return colum;
+	}
+	//释放资源
+	public void close(){
+		if(rs!=null){
+			try {
+				rs.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		if(ps!=null){
+			try {
+				ps.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		if(con!=null){
+			try {
+				con.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+}
